15a875e792fc34a18ed9f41ff7b335ff97dfbddd
Alex Esson <Ghostshadow1222@users.noreply.github.com>
2025-08-13 18:50:24 -0700

    Add Account class for managing bank transactions
    Introduces the `Account` class in the `BankAccount` namespace, featuring properties for `AccountNumber` and `Balance`. Implements `Deposit` and `Withdraw` methods with validation to ensure proper transaction handling. This class lays the foundation for basic banking operations.

---
+++ BankAccount/Account.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    /// <summary>
+    /// Represents an individual bank account
+    /// </summary>
+    public class Account
+    {
+        /// <summary>
+        /// AccountNumbers must start with 4 diggits followed by a dash and then 5 characters (A - Z) not case sensitive
+        /// </summary>
+        public required string AccountNumber { get; set; }
+
+        /// <summary>
+        /// The current balance of the account
+        /// </summary>
+        public decimal Balance { get; private set; }
+
+        /// <summary>
+        /// Deposits money into the account then returns the new balance
+        /// </summary>
+        /// <param name="amount">The amount to deposit. Must be a positive value.</param>
+        /// <returns>The updated account balance after the deposit</returns>
+        public decimal Deposit (decimal amount)
+        {
+            ArgumentOutOfRangeException.ThrowIfNegative(amount);
+            
+            Balance += amount;
+            return Balance;
+        }
+
+        /// <summary>
+        /// Withdraws money from the account
+        /// </summary>
+        /// <param name="amount">The amount to withdraw. Must be positive value.</param>
+        /// <returns>The updated account balance after the withdrawal.</returns>
+        public decimal Withdraw (decimal amount)
+        {
+            if (amount <= 0 || amount > Balance)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount),"Cannot withdraw a negative amount or more than the current balance.");
+            }
+
+            Balance -= amount;
+            return Balance;
+        }
+
+
+    }
+}
